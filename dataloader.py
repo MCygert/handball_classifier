c7e9313f08dbd3e9ca18f3d252fe31b191c8bc5a
Mikolaj Cygert <cygertmikolaj@gmail.com>
2021-10-16 19:33:48 +0200

    Add data loading

--- dataloader.py
+++ dataloader.py
@@ -2,13 +2,14 @@ import cv2
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import torch
+from torchvision import transforms
 
 
-def get_frames(filename, n_frames=1):
+def get_frames(filename, n_frames=3):
     frames = []
     v_cap = cv2.VideoCapture(filename)
     v_len = int(v_cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_list = np.linspace(0, v_len - 1, n_frames + 1, dtype=np.int16)
+    frame_list = np.linspace(0, v_len - 1, v_len // n_frames, dtype=np.int16)
 
     for fn in range(v_len):
         success, frame = v_cap.read()
@@ -16,17 +17,20 @@ def get_frames(filename, n_frames=1):
             continue
         if fn in frame_list:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frames.append(frame)
+            asarray = np.asarray(frame)
+            frames.append(asarray)
 
     v_cap.release()
-    return frames, v_len
+    # Change dimensions to Frames x Channel x Height x Width
+    np_asarray = np.transpose(np.asarray(frames),  (0,3,2,1))
+    return np_asarray,  v_len
 
 
 class VideoDataSet(Dataset):
-    def __init__(self, all_video_file, transformers):
+    def __init__(self, all_video_file):
         # This maps csv which has file path and label to numpy arrray 
-        self.videos = np.genfromtxt(all_video_file, delimeters=",", dtype=np.unicode_)
-        self.transformers = transformers
+        self.videos = np.genfromtxt(all_video_file, delimiter=",", dtype=np.unicode_)
+        #self.transformers = transformers
 
     def __len__(self):
         return len(self.videos)
@@ -37,8 +41,17 @@ class VideoDataSet(Dataset):
         frames_torch = []
 
         for frame in frames:
-            frame = self.transformers(frame)
+            #frame = self.transformers(frame)
             frames_torch.append(frame)
         if len(frames_torch) > 0:
             frames_torch = torch.stack(frames_torch)
         return frames_torch, label
+
+
+data_transform = transforms.Compose([
+    transforms.Resize((720, 720)),
+    transforms.ToTensor(),
+
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+])
---
+++ test_data_loader.py
@@ -0,0 +1,5 @@
+from dataloader import VideoDataSet 
+
+ds = VideoDataSet("data/videos.csv")
+
+
